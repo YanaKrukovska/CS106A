9f42fc4b1969f21b15909698fb42485840e414b7
v.krukovskyi <v.krukovskyy@gmail.com>
2018-09-30 22:17:06 +0300

    Initial commit

--- Homework4_2.java
+++ Homework4_2.java
@@ -2,102 +2,103 @@ import stanford.karel.*;
 
 public class Homework4_2 extends SuperKarel {
 
-	public void run() {
-		if (frontIsBlocked()) {
-			putBeeper();
-		} else {
-			fillFirstRow();
-			while (frontIsClear()) {
-				goToStart();
-				markWestBorder();
-				fillNextRow();
+    public void run() {
+        if (frontIsBlocked()) {
+            putBeeper();
 
-				turnAround();
-				if (rightIsClear()) {
+        } else {
+            fillFirstRow();
+            while (frontIsClear()) {
+                goToStart();
+                markWestBorder();
+                fillNextRow();
 
-					move();
-					turnRight();
-					move();
-					if (beepersPresent()) {
-						break;
-					} else {
-						turnAround();
-						move();
-						turnLeft();
-						move();
-					}
-				} else {
-					goToStart();
-					fillNextRow();
-					break;
-				}
+                turnAround();
+                if (rightIsClear()) {
 
-				markEastBorder();
-			}
-		}
-	}
+                    move();
+                    turnRight();
+                    move();
+                    if (beepersPresent()) {
+                        break;
+                    } else {
+                        turnAround();
+                        move();
+                        turnLeft();
+                        move();
+                    }
+                } else {
+                    goToStart();
+                    fillNextRow();
+                    break;
+                }
 
-	private void fillNextRow() {
-		if (beepersPresent()) {
-			move();
-		}
-		while (noBeepersPresent()) {
-			putBeeper();
-			move();
+                markEastBorder();
+            }
+        }
+    }
 
-		}
+    private void fillNextRow() {
+        if (beepersPresent()) {
+            move();
+        }
+        while (noBeepersPresent()) {
+            putBeeper();
+            move();
 
-	}
+        }
 
-	private void goToStart() {
+    }
 
-		do {
-			move();
+    private void goToStart() {
 
-		} while (noBeepersPresent());
-		turnAround();
+        do {
+            move();
 
-	}
+        } while (noBeepersPresent());
+        turnAround();
 
-	private void fillFirstRow() {
+    }
 
-		putBeeper();
-		markWestBorder();
-		while (frontIsClear()) {
-			if (noBeepersPresent()) {
-				putBeeper();
-			}
-			move();
-		}
-		putBeeper();
-		if (leftIsClear()) {
-			markEastBorder();
-		}
+    private void fillFirstRow() {
 
-	}
+        putBeeper();
+        markWestBorder();
+        while (frontIsClear()) {
+            if (noBeepersPresent()) {
+                putBeeper();
+            }
+            move();
+        }
+        putBeeper();
+        if (leftIsClear()) {
+            markEastBorder();
+        }
 
-	private void markEastBorder() {
+    }
 
-		turnAround();
-		if (rightIsClear()) {
-			move();
-			turnRight();
-			move();
-			putBeeper();
-			turnLeft();
-		}
-	}
+    private void markEastBorder() {
 
-	private void markWestBorder() {
-		if (frontIsClear() && leftIsClear()) {
-			move();
-			turnLeft();
-			move();
-			putBeeper();
-			turnAround();
-			move();
-			turnLeft();
-		}
-	}
+        turnAround();
+        if (rightIsClear()) {
+            move();
+            turnRight();
+            move();
+            putBeeper();
+            turnLeft();
+        }
+    }
+
+    private void markWestBorder() {
+        if (frontIsClear() && leftIsClear()) {
+            move();
+            turnLeft();
+            move();
+            putBeeper();
+            turnAround();
+            move();
+            turnLeft();
+        }
+    }
 
 }
