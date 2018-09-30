f1e376c376a73a2566d8d80748eeef5c2fad3950
v.krukovskyi <v.krukovskyy@gmail.com>
2018-09-30 22:47:12 +0300

    fixed class file name

---
+++ FirstProgram.java
@@ -0,0 +1,12 @@
+/*
+ * Це заготовка для першого практичного зайняття
+ * світ має назву lection1
+ */
+import stanford.karel.*;
+
+public class FirstProgram extends Karel{
+
+	public void run(){
+		
+	}
+}
